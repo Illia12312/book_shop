b78020428bd7267e4ddb485edee96b09009e0e94
Illia Rak <illiarak609@gmail.com>
2023-05-03 19:55:00 +0200

    range-slider

--- src/pages/Kid/Kid.jsx
+++ src/pages/Kid/Kid.jsx
@@ -5,12 +5,23 @@ import Card from "../../components/Card/Card";
 import { useState } from "react";
 import _ from "lodash";
 import DiscountCard from "../../components/DiscountCard/DiscountCard";
+import Slider from '@mui/material/Slider';
+import Box from '@mui/material/Box';
 
+
+function valuetext(value) {
+  return `${value}°C`;
+}
 
 const Kid = () =>{
     const [item, setItem] = useState([]);
+    const [value, setValue] = useState([60,345])
+    const handleChange = (event, newValue) => {
+      setValue(newValue);
+    };
 
     const books = useSelector((state) => state.books.books)
+    const booksType = books.filter((i) => i.type === "kid")
 
     useEffect(() => {
         setItem(books);
@@ -28,9 +39,26 @@ const Kid = () =>{
     }
     }
 
+    useEffect(() => {
+      console.log(value, booksType);
+    }, value);
+
     return(
         <div className="kidHolder">
           <h3 className="kidTitle">Для Детей</h3>
+          <Box sx={{ width: 180 }}>
+            <Slider
+            getAriaLabel={() => 'Minimum distance'}
+            value={value}
+            onChange={handleChange}
+            valueLabelDisplay="auto"
+            getAriaValueText={valuetext}
+            disableSwap
+            max={345}
+            min={60}
+            color="secondary"
+            />
+          </Box>
           <div className="kid">
           <select
           name="select"
